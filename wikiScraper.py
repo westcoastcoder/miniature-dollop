a7082fedb66d116308a2d511164a9cfbdeb6c712
westcoastcoder <wwscottbc@gmail.com>
2020-10-10 00:25:44 -0700

    looked into ssl and made changes to experiment with verification requests

--- wikiScraper.py
+++ wikiScraper.py
@@ -2,18 +2,30 @@
 # the help of a FCC blog post and Dr. Chuck's book
 
 from bs4 import BeautifulSoup
+import request
 import urllib.request
 import urllib.parse
 import urllib.error
 import ssl
+# import random
 
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+#
+
+# I do not fully understand this code as it is from Dr. Chuck's book.
+# It looks like this may be replaced with one line of code
+# ctx = ssl.create_default_context()
+# ctx.check_hostname = False
+# ctx.verify_mode = ssl.CERT_NONE
 
 url = input()
 
-html = urllib.request.urlopen(url, context=ctx).read()
+# html = urllib.request.urlopen(url, context=ctx).read()
+# test code re: above comment regarding ssl
+# update: this works. Need to read ssl documentation to understand why
+
+# OK. Still some learning to do. This is not validating the source, which on a
+# larger scale would create security issues.
+html = urllib.request.urlopen(url, context=ssl._create_unverified_context())
 
 soup = BeautifulSoup(html, 'html.parser')
 
@@ -38,6 +50,6 @@ for tag in tags:
         continue
 
 
-print(potentialLinks)
+print(potentialLinks[0])
 print('----------------------------------------')
 print(badLinks)
